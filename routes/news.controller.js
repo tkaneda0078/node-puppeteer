fc356def77e50d8bb885563ed27ff4ca4487bd76
tkaneda0078 <1111510078r21@gmail.com>
2018-10-01 10:18:25 +0900

    各urlをconfigから取得するように修正

--- routes/news.controller.js
+++ routes/news.controller.js
@@ -2,16 +2,22 @@
 
 const express = require('express')
 const router = express.Router()
+var config = require('config');
 // const News = require('../models/news')
 const WotopiMedia = require('../models/wotopiMedia')
 
 router.get('/:category', function (req, res, next) {
   (async () => {
-    let baseUrl = 'https://wotopi.jp/archives/category/'
-    let url = baseUrl + req.params.category
-    const wotopi = await new WotopiMedia()
-    await wotopi.build(url)
-    await wotopi.scraping()
+    let category = req.params.category
+    // 各メディアのURLを取得
+    let mediaUrl = config.get(`Media.${category}`)
+    // todo 動的に
+    for (let key in mediaUrl) {
+      let url = mediaUrl[key] + category
+      const wotopi = await new WotopiMedia()
+      await wotopi.build(url)
+      await wotopi.scraping()
+    }
   })()
 })
 
