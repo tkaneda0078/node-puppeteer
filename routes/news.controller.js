d005a7f8aebb785df9b97a066580f35e3fa3ac0a
tkaneda0078 <1111510078r21@gmail.com>
2018-10-01 10:56:05 +0900

    request status code 確認

--- routes/news.controller.js
+++ routes/news.controller.js
@@ -3,6 +3,7 @@
 const express = require('express')
 const router = express.Router()
 var config = require('config');
+const request = require('request');
 // const News = require('../models/news')
 const WotopiMedia = require('../models/wotopiMedia')
 
@@ -14,11 +15,25 @@ router.get('/:category', function (req, res, next) {
     // todo 動的に
     for (let key in mediaUrl) {
       let url = mediaUrl[key] + category
+      if (!validateIsRequestStatus(url)) {
+        continue
+      }
+
       const wotopi = await new WotopiMedia()
       await wotopi.build(url)
       await wotopi.scraping()
     }
   })()
 })
+
+function validateIsRequestStatus(url) {
+  request(url, function (error, response) {
+    if (!error && response.statusCode == 200) {
+      return true
+    } else {
+      return false
+    }
+  })
+}
 
 module.exports = router
